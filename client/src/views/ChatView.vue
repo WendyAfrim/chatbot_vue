09773bf53cc1017da19bacee217e19962a81e213
jeremdh62 <jdhaille@myges.fr>
2022-12-15 14:35:57 +0100

    add chat

--- client/src/views/ChatView.vue
+++ client/src/views/ChatView.vue
@@ -4,21 +4,74 @@
 
     const socket = io("ws://localhost:3000");
 
+    socket.on("connect", () => {
+        console.log("Connected to the socket")
+    })
 
+    //  on ouvre une connexion socket
+    window.onload = () => {
+        document.querySelector("form").addEventListener('submit', (e) => {
+            e.preventDefault();
+            console.log(e)
+
+            const name = document.querySelector('#name');
+            const message = document.querySelector('#message');
+
+            socket.emit('chat_message', {name: name.value, message: message.value})
+            document.querySelector('#message').value = '';
+
+        }) 
+
+        socket.on('chat_message', (msg) => {
+            document.querySelector('#messages').innerHTML += `<p>${ msg.name }: ${msg.message}</p>`
+        });
+
+    }
 </script>
 
 <template>
-    <div class="about">
-        <h1>This is an about page</h1>
-    </div>
+    <div id="messages"></div>
+    <form>
+        <input type="text" id="name" placeholder="Entrez votre nom">
+        <input type="text" id="message" placeholder="Entrez votre message">
+        <button>Envoyer</button>
+    </form>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-    .about {
-        min-height: 100vh;
-        display: flex;
-        align-items: center;
+#messages {
+        border: 0.1em solid black;
+        height: 90vh;
     }
-}
+
+    #messages p {
+        margin: 0;
+        padding: 10px;
+    }
+
+    #messages :nth-child(odd) {
+        background-color: #aaa;
+    }
+
+    form {
+        display: flex;
+    }
+
+    #name {
+        flex: 1;
+        padding: 10px;
+    }
+
+    #message {
+        flex: 5;
+        padding: 10px;
+    }
+
+    button {
+        flex: 1;
+        padding: 10px;
+        border: none;
+        cursor: pointer;
+        background-color: #eee;
+    }
 </style>
